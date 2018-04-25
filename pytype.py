ab35122926e35303cf836855b8b01c4957ff94b2
Sean Joseph <joseph62@egr.msu.edu>
2018-04-25 19:46:04 -0400

    Type object in the __mro__ of a type is no longer included in matching. Also added HasAttr and NotNone to show some ways to enforce restrictions other than just matching type.

--- pytype.py
+++ pytype.py
@@ -21,24 +21,53 @@ def pytype(*type_args,**type_kwargs):
         types_ = dict(zip(varnames,type_args))
         types_.update(type_kwargs)
         def wrapped_func(*args,**kwargs):
-            def any_type_in(possible_types,target_type):
-                return any(t == tt for t in possible_types 
-                                    for tt in target_type.__mro__)
             nonlocal types_
             nonlocal varnames
             nonlocal func
+            # Are any of the possible types in the target type's inheritance?
+            def any_type_in(possible_types,target_type):
+                return any(poss == target for poss in possible_types 
+                                    for target in target_type.__mro__ if target != object)
             # Create a dict of varname to given value
             values = dict(zip(varnames,args))
             values.update(kwargs)
             # All argument values must match up to the possible types given
             for name,value in values.items():
-                assert any_type_in(types_[name],type(value)),"{} must be one of types {} it actually is {}!".format(name,types_[name],type(value))
+                possible_types = types_[name]
+                value_type = type(value)
+                if not any_type_in(possible_types,value_type):
+                    raise TypeError("Argument {}('{}') does not match any of {}!".format(
+                                name, value_type, list(possible_types)))
             result = func(*args,**kwargs)
             return result 
         return wrapped_func
     return _pytype
 
+class HasAttr:
+    """
+    Give a string identifier for an attribute that the field requires
+    """
+    def __init__(self,attr):
+        self.attr = attr
+    def __eq__(self,other):
+        return self.attr in dir(other)
+    def __str__(self):
+        return "HasAttr('{}')".format(self.attr)
+    __repr__ = __str__
+
+class NotNone:
+    """
+    Not None
+    """
+    def __eq__(self,other):
+        return other != type(None)
+    def __str__(self):
+        return "NotNone"
+    __repr__ = __str__
+
+
 if __name__ == "__main__":
+
     class A:
         pass
     class B(A):
@@ -50,12 +79,6 @@ if __name__ == "__main__":
         return True
     inheritance_check(B(),A())
 
-    class HasAttr:
-        def __init__(self,attr):
-            self.attr = attr
-        def __eq__(self,other):
-            return self.attr in dir(other)
-
     @pytype(HasAttr("__iter__"),int)
     def window(iterable,size):
         return list(zip(*([iter(iterable)]*size)))
@@ -63,11 +86,21 @@ if __name__ == "__main__":
     print(window(range(14),5))
     # window(1,2) -- raises exception
 
+    @pytype(NotNone())
+    def id(thing):
+        return thing
+
+    print(id(1))
+    try:
+        id(None)
+    except TypeError as e:
+        print(e)
+
     @pytype(int,int)
     def add(a,b):
         return a + b
     print(add(1,2))
     try:
         add(1.0,2.0)
-    except Exception as e:
-        print("Type Error:",e)
+    except TypeError as e:
+        print(e)
