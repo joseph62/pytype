56b7ff3459cd01a4d3cda0fca7d458ec8546440a
Sean Joseph <theseanjoseph@gmail.com>
2019-10-15 20:19:42 -0400

    Applied black formatting

--- pytype.py
+++ pytype.py
@@ -2,10 +2,12 @@ from inspect import signature
 from functools import wraps
 from abc import ABC, abstractmethod
 
+
 class PyTypeValidator(ABC):
     @abstractmethod
     def validate(self, value) -> bool:
         pass
+
 
 def _any_type_in(input_value, target_type):
     if isinstance(target_type, PyTypeValidator):
@@ -13,30 +15,50 @@ def _any_type_in(input_value, target_type):
     else:
         return any(target_type == t for t in type(input_value).mro()[:-1])
 
+
 def _name_value_annotation(bound_arguments, signature):
-    return ((parameter.name, bound_arguments.arguments[parameter.name], parameter.annotation)
-                for parameter in signature.parameters.values())
+    return (
+        (
+            parameter.name,
+            bound_arguments.arguments[parameter.name],
+            parameter.annotation,
+        )
+        for parameter in signature.parameters.values()
+    )
+
+
 def pytype(f):
+    """
+    Validate the arguments of the supplied function with its annotations.
+    """
     f_signature = signature(f)
+
     @wraps(f)
-    def _pytype(*args,**kwargs):
+    def _pytype(*args, **kwargs):
         nonlocal f
         nonlocal f_signature
         bound_arguments = f_signature.bind(*args, **kwargs)
         argument_errors = []
-        for name, value, annotation in _name_value_annotation(bound_arguments, f_signature):
+        for name, value, annotation in _name_value_annotation(
+            bound_arguments, f_signature
+        ):
             if not _any_type_in(value, annotation):
                 argument_errors.append(
                     f'Argument {name}("{annotation}") does not match any of {type(value).mro()[:-1]}!'
                 )
         if argument_errors:
-            raise TypeError('\n'.join(argument_errors))
-        result = f(*args,**kwargs)
+            raise TypeError("\n".join(argument_errors))
+        result = f(*args, **kwargs)
         return result
 
     return _pytype
 
+
 class NotZero(PyTypeValidator):
+    """
+    Validate that a value is an int or a float and that it is not zero
+    """
+
     def validate(self, value):
         return (isinstance(value, int) or isinstance(value, float)) and value != 0
 
@@ -45,20 +67,28 @@ class HasAttr:
     """
     Give a string identifier for an attribute that the field requires
     """
-    def __init__(self,attr):
+
+    def __init__(self, attr):
         self.attr = attr
-    def __eq__(self,other):
+
+    def __eq__(self, other):
         return self.attr in dir(other)
+
     def __str__(self):
         return "HasAttr('{}')".format(self.attr)
+
     __repr__ = __str__
+
 
 class NotNone:
     """
     Not None
     """
-    def __eq__(self,other):
+
+    def __eq__(self, other):
         return other != type(None)
+
     def __str__(self):
         return "NotNone"
+
     __repr__ = __str__
--- pytype_test.py
+++ pytype_test.py
@@ -1,6 +1,7 @@
 import unittest
 from pytype import pytype, HasAttr, NotNone, NotZero
 
+
 class TestPytype(unittest.TestCase):
     def assertNotRaises(self, exception, f, *args, **kwargs):
         error = False
@@ -8,36 +9,41 @@ class TestPytype(unittest.TestCase):
             f(*args, **kwargs)
         except exception:
             error = True
-        assert not error, f'{exception.__name__} was raised by {f.__name__}'
+        assert not error, f"{exception.__name__} was raised by {f.__name__}"
 
     def test_inheritance(self):
         class A:
             pass
+
         class B(A):
             pass
 
         @pytype
-        def inheritance_check(a: A,b: A):
+        def inheritance_check(a: A, b: A):
             return True
+
         self.assertNotRaises(TypeError, inheritance_check, B(), A())
 
     def test_has_attr(self):
         @pytype
-        def window(iterable: HasAttr('__iter__'), size: int):
-            return list(zip(*([iter(iterable)]*size)))
-        self.assertNotRaises(TypeError, window, [1,2], 1)
+        def window(iterable: HasAttr("__iter__"), size: int):
+            return list(zip(*([iter(iterable)] * size)))
+
+        self.assertNotRaises(TypeError, window, [1, 2], 1)
         self.assertRaises(TypeError, window, 5, 4)
 
     def test_not_none_raises_error(self):
         @pytype
         def id(thing: NotNone()):
             return thing
+
         self.assertRaises(TypeError, id, None)
 
     def test_not_none(self):
         @pytype
         def id(thing: NotNone()):
             return thing
+
         self.assertRaises(TypeError, id, None)
         self.assertNotRaises(TypeError, id, 1)
 
@@ -45,11 +51,13 @@ class TestPytype(unittest.TestCase):
         @pytype
         def division(x, y: NotZero()):
             return x / y
+
         self.assertRaises(TypeError, division, 1, 0)
 
     def test_raw_type(self):
         @pytype
         def add(a: int, b: int):
             return a + b
+
         self.assertNotRaises(TypeError, add, 1, 2)
         self.assertRaises(TypeError, add, 1.0, 2.0)
